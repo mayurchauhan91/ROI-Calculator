45031d23bb3492fc54ade26b98ff0f547828edb8
Mayur Chauhan <mayurchauhan@Mayurs-MacBook-Pro.local>
2022-05-08 15:34:04 +0100

    header update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const sum = 1 + 1;
   return (
     <div className="App">
-      <h1 className="header">ROI Calulator</h1>
+      <h1 className="header">ROI</h1>
       <div className="main-container">
         <Input />
         <Result sum={sum} />
